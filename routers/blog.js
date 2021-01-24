ba7669b443ae63d3e55e4681ba2e6418f92670b7
AmrAymanAboelanwar <77896593+AmrAymanAboelanwar@users.noreply.github.com>
2021-01-24 19:57:08 +0200

    images

--- routers/blog.js
+++ routers/blog.js
@@ -8,15 +8,13 @@ const path = require('path');
 
 
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-      cb(null, 'images');
-  },
-
+  destination: path.join(__dirname,"images"),
   // By default, multer removes file extensions so let's add them back
   filename: function(req, file, cb) {
       cb(null, file.originalname + '-' + Date.now() + path.extname(file.originalname));
   }
 });
+
 
 const upload = multer({ storage: storage });
 
